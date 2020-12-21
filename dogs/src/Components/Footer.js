f3dd09d9323878d619c69a85e055e1a853b4c419
cleytonchagasbr <cleyton-chagas@hotmail.com>
2020-12-21 20:11:54 -0300

    adicionando Header

---
+++ dogs/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={styled.footer}>
+            Footer
+        </div>
+    )
+}
+
+export default Footer
